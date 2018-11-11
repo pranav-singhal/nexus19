a4490c26743ec9c3e6f0f1ca249bd4615ca3ad27
Pranav Singhal <pranavsinghal96@gmail.com>
2018-11-11 20:23:37 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// var sass = require('node-sass');
+const express = require('express');
+var browserSync = require('browser-sync');
+var bs = browserSync.create().init({ logSnippet: false });
+
+var path = require('path');
+bourbon  = require("bourbon").includePaths;
+sassMiddleware = require('node-sass-middleware');
+const app = express();
+const port = 3000;
+// console.log(__dirname);
+app.use(
+    sassMiddleware({
+        src: __dirname + '/scss',
+        dest: __dirname + '/app',
+        debug: true,
+        includePaths: [bourbon]
+    })
+ );
+ app.use(require('connect-browser-sync')(bs));
+
+
+app.use('/', express.static('app'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
