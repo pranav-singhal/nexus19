0f4225eef8513aec536144e59b0e961b956360f2
Pranav Singhal <pranavsinghal96@gmail.com>
2018-12-22 15:48:50 +0530

    added firebase

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 // var sass = require('node-sass');
 const express = require('express');
+const firebase = require('firebase');
+require('firebase/firestore');
+
+var config = {
+  apiKey: 'AIzaSyDyC96e22W83WWLGVszg1g-D5_cX6aEEHQ',
+  authDomain: 'nexus19-zombie.firebaseapp.com',
+  databaseURL: 'https://nexus19-zombie.firebaseio.com',
+  projectId: 'nexus19-zombie',
+  storageBucket: 'nexus19-zombie.appspot.com',
+  messagingSenderId: '58844651501'
+};
+firebase.initializeApp(config);
+var database = firebase.firestore();
+const settings = { /* your settings... */ timestampsInSnapshots: true };
+database.settings(settings);
+
+var document = database.collection('event-details').doc('FLyCv8uputIwdBq380lD');
+// doc.get().then(doc => {
+//   console.log(doc.data().acapella);
+// });
 // var browserSync = require('browser-sync');
 // var bs = browserSync.create();
 // bs.init({ logSnippet: false});
@@ -51,7 +71,13 @@ app.get('/past-sponsors', (req, res) => {
   res.render('pages/past-sponsors');
 });
 app.get('/get_modal_content', (req, res) => {
-  res.render('partials/modal', { layout: false });
+  console.log(req.query);
+  const content = req.query.modal_name;
+  document.get().then(doc => {
+    console.log(typeof doc.data()[content]);
+    modal_content = doc.data()[content];
+    res.render('partials/modal', { content: modal_content });
+  });
 });
 app.listen(process.env.PORT || port, '0.0.0.0', () =>
   console.log(`nexus19 listening on port ${port}!`)
