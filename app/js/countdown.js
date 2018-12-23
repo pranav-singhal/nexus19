9f0b91df415b8fac61a346bf34f59b001fd7eb7f
Pranav Singhal <pranavsinghal96@gmail.com>
2018-12-23 19:21:11 +0530

    aded countdown

---
+++ app/js/countdown.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('#DateCountdown').TimeCircles({
+    animation: 'smooth',
+    bg_width: 0.5,
+    fg_width: 0.023333333333333334,
+    circle_bg_color: 'grey',
+    time: {
+      Days: {
+        text: 'Days',
+        color: 'white',
+        show: true
+      },
+      Hours: {
+        text: 'Hours',
+        color: 'white',
+        show: true
+      },
+      Minutes: {
+        text: 'Minutes',
+        color: 'white',
+        show: true
+      },
+      Seconds: {
+        text: 'Seconds',
+        color: 'white',
+        show: false
+      }
+    }
+  });
+
+  console.log('in countdown js');
+  $('#DateCountdown')
+    .TimeCircles()
+    .rebuild();
+});
